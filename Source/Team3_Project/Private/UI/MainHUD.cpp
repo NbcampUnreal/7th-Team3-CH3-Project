4a59dcfe8da6e5f0ccb5c9cba1c078acd71aae8d
challenge04su <challenge04su@gmail.com>
2026-02-12 22:12:34 +0900

    MainHUD_h/cpp
    TotalScore, TotalKills, Stamina, Health, Timer, WhiteKarma, BlackKarma

--- Source/Team3_Project/Private/UI/MainHUD.cpp
+++ Source/Team3_Project/Private/UI/MainHUD.cpp
@@ -1,2 +1,169 @@
 #include "UI/MainHUD.h"
+#include "Components/TextBlock.h"
+#include "Components/ProgressBar.h"
 
+UMainHUD::UMainHUD(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+    InterpSpeed = 5.0f;
+
+    CurrHealth = 1.0f;
+    TargetHealth = 1.0f;
+
+    CurrStamina = 1.0f;
+    TargetStamina = 1.0f;
+
+    CurrWhiteKarma = 0.5f;
+    TargetWhiteKarma = 0.5f;
+    CurrBlackKarma = 0.5f;
+    TargetBlackKarma = 0.5f;
+}
+
+void UMainHUD::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    if (Timer)
+    {
+        Timer->SetVisibility(ESlateVisibility::Collapsed);
+    }
+}
+
+void UMainHUD::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+    Super::NativeTick(MyGeometry, InDeltaTime);
+
+    if (PB_Health)
+    {
+        CurrHealth = FMath::FInterpTo(CurrHealth, TargetHealth, InDeltaTime, InterpSpeed);
+        PB_Health->SetPercent(CurrHealth);
+        UpdateDynamicColor(PB_Health, CurrHealth);
+    }
+
+    if (PB_Stamina)
+    {
+        CurrStamina = FMath::FInterpTo(CurrStamina, TargetStamina, InDeltaTime, InterpSpeed);
+        PB_Stamina->SetPercent(CurrStamina);
+        UpdateDynamicColor(PB_Stamina, CurrStamina); 
+    }
+
+    if (PB_WhiteKarma)
+    {
+        CurrWhiteKarma = FMath::FInterpTo(CurrWhiteKarma, TargetWhiteKarma, InDeltaTime, InterpSpeed);
+        PB_WhiteKarma->SetPercent(CurrWhiteKarma);
+    }
+    if (PB_BlackKarma)
+    {
+        CurrBlackKarma = FMath::FInterpTo(CurrBlackKarma, TargetBlackKarma, InDeltaTime, InterpSpeed);
+        PB_BlackKarma->SetPercent(CurrBlackKarma);
+    }
+}
+
+void UMainHUD::UpdateDynamicColor(UProgressBar* Bar, float CurrentPercent)
+{
+    if (!Bar) return;
+
+    FLinearColor TargetColor;
+    if (CurrentPercent <= 0.2f)
+    {
+        TargetColor = FLinearColor::Red;
+    }
+    else if (CurrentPercent <= 0.6f)
+    {
+        float Alpha = (CurrentPercent - 0.2f) / 0.4f;
+        TargetColor = FLinearColor::LerpUsingHSV(FLinearColor::Red, FLinearColor::Yellow, Alpha);
+    }
+    else
+    {
+        float Alpha = (CurrentPercent - 0.6f) / 0.4f;
+        TargetColor = FLinearColor::LerpUsingHSV(FLinearColor::Yellow, FLinearColor::Green, Alpha);
+    }
+    Bar->SetFillColorAndOpacity(TargetColor);
+}
+
+void UMainHUD::UpdateScore(int32 Score)
+{
+    if (TotalScore)
+    {
+        TotalScore->SetText(FText::Format(FText::FromString("Score : {0}"), Score));
+    }
+}
+
+void UMainHUD::UpdateKills(int32 kills)
+{
+    if (TotalKills)
+    {
+        TotalKills->SetText(FText::Format(FText::FromString("Kill : {0}"), kills));
+    }
+}
+
+void UMainHUD::UpdateHealth(float Health)
+{
+    TargetHealth = Health;
+}
+
+void UMainHUD::UpdateStamina(float Stamina)
+{
+    /*if (PB_Stamina)
+    {
+        PB_Stamina->SetPercent(Stamina);
+
+        if (Stamina <= 0.3f)
+        {
+            PB_Stamina->SetFillColorAndOpacity(FLinearColor::Red);
+        }
+        else if (Stamina <= 0.6f)
+        {
+            PB_Stamina->SetFillColorAndOpacity(FLinearColor::Yellow);
+        }
+        else
+        {
+            PB_Stamina->SetFillColorAndOpacity(FLinearColor::Green);
+        }
+    }*/
+    TargetStamina = Stamina;
+}
+
+void UMainHUD::UpdateWhiteKarma(float WhiteKarma)
+{
+   /* if (PB_WhiteKarma)
+    {
+        PB_WhiteKarma->SetPercent(WhiteKarma);
+    }*/
+    TargetWhiteKarma = WhiteKarma;
+}
+
+void UMainHUD::UpdateBlackKarma(float BlackKarma)
+{
+    /*if (PB_BlackKarma)
+    {
+        PB_BlackKarma->SetPercent(BlackKarma);
+    }*/
+    TargetBlackKarma = BlackKarma;
+}
+
+void UMainHUD::UpdateTime(float Time)
+{
+    if (Timer)
+    {
+        if (Timer->GetVisibility() != ESlateVisibility::Visible)
+        {
+            Timer->SetVisibility(ESlateVisibility::Visible);
+        }
+
+        int32 Minutes = FMath::FloorToInt(Time / 60.0f);
+        int32 Seconds = FMath::FloorToInt(FMath::Fmod(Time, 60.0f));
+
+        FString TimeString = FString::Printf(TEXT("%02d : %02d"), Minutes, Seconds);
+
+        Timer->SetText(FText::FromString(TimeString));
+
+        if (Time <= 10.0f)
+        {
+            Timer->SetColorAndOpacity(FSlateColor(FLinearColor::Red));
+        }    
+        else
+        {
+            Timer->SetColorAndOpacity(FSlateColor(FLinearColor::White));
+        }
+    }
+}
--- Source/Team3_Project/Public/UI/MainHUD.h
+++ Source/Team3_Project/Public/UI/MainHUD.h
@@ -2,11 +2,71 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/ProgressBar.h"
 #include "MainHUD.generated.h"
 
 UCLASS()
 class TEAM3_PROJECT_API UMainHUD : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+    UMainHUD(const FObjectInitializer& ObjectInitializer);
+
+protected:
+    virtual void NativeConstruct() override;
+    virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* TotalScore;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* TotalKills;
+
+    UPROPERTY(meta = (BindWidget))
+    class UProgressBar* PB_Health;
+
+    UPROPERTY(meta = (BindWidget))
+    class UProgressBar* PB_Stamina;
+
+    UPROPERTY(meta = (BindWidget))
+    class UProgressBar* PB_WhiteKarma;
+
+    UPROPERTY(meta = (BindWidget))
+    class UProgressBar* PB_BlackKarma;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* Timer;
+
+    UPROPERTY(EditAnywhere, Category = "UI Style")
+    float InterpSpeed;
+
+    float CurrHealth;
+    float TargetHealth;
+
+    float CurrStamina;
+    float TargetStamina;
+
+    float CurrWhiteKarma;
+    float TargetWhiteKarma;
+    float CurrBlackKarma;
+    float TargetBlackKarma;
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "HUD Test")
+    void UpdateScore(int32 Score);
+    UFUNCTION(BlueprintCallable, Category = "HUD Test")
+    void UpdateKills(int32 Kills);
+    UFUNCTION(BlueprintCallable, Category = "HUD Test")
+    void UpdateHealth(float Health);
+    UFUNCTION(BlueprintCallable, Category = "HUD Test")
+    void UpdateStamina(float Stamina);
+    UFUNCTION(BlueprintCallable, Category = "HUD Test")
+    void UpdateWhiteKarma(float WhiteKarma);
+    UFUNCTION(BlueprintCallable, Category = "HUD Test")
+    void UpdateBlackKarma(float BlackKarma);
+    UFUNCTION(BlueprintCallable, Category = "HUD Test")
+    void UpdateTime(float Time);
+
+    void UpdateDynamicColor(class UProgressBar* Bar, float CurrentPercent);
 };
