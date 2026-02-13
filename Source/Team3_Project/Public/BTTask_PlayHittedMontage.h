0c80628ac98d0a9be2b2b1cbe74d17c10f2cf165
정찬호_JCH <elvpfhhrm@gmail.com>
2026-02-13 15:56:03 +0900

    BehaviorTree Task 추가
    BehaviorTree에서 사용할 Task 클래스 파일을 생성했습니다.

---
+++ Source/Team3_Project/Public/BTTask_PlayHittedMontage.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_PlayHittedMontage.generated.h"
+
+UCLASS()
+class TEAM3_PROJECT_API UBTTask_PlayHittedMontage : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
