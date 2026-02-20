ad44aa1b5ddf0f69052409ada3726d379c8b89b8
Daype1015 <mj1527123@gmail.com>
2026-02-20 14:28:49 +0900

    서브레벨 전환 프로토타입 구현

---
+++ Source/Team3_Project/Private/Core/LevelTestActor.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/LevelTestActor.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+
+// Sets default values
+ALevelTestActor::ALevelTestActor()
+{
+	OverlapVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapVolume"));
+	RootComponent = OverlapVolume;
+	PrimaryActorTick.bCanEverTick = false;
+
+	OverlapVolume->OnComponentBeginOverlap.AddUniqueDynamic(this, &ALevelTestActor::OverlapBegins);
+
+}
+
+// Called when the game starts or when spawned
+void ALevelTestActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
+void ALevelTestActor::OverlapBegins(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult
+)
+{
+	ACharacter* CurrentCharacter = UGameplayStatics::GetPlayerCharacter(this, 0);
+
+	if ((OtherActor == CurrentCharacter) && LevelToLoad != "")
+	{
+		FLatentActionInfo LatentInfo;
+		UGameplayStatics::LoadStreamLevel(this, LevelToLoad, true, true, LatentInfo);
+	}
+}
+
---
+++ Source/Team3_Project/Public/Core/LevelTestActor.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LevelTestActor.generated.h"
+
+UCLASS()
+class TEAM3_PROJECT_API ALevelTestActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ALevelTestActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+
+protected:
+
+	UFUNCTION()
+	void OverlapBegins(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+	UPROPERTY(EditAnywhere)
+	FName LevelToLoad;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	TObjectPtr<class UBoxComponent> OverlapVolume;
+
+};
