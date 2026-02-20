80bf9b1e6db8a552ec52c1f2680c1fadb2d7fbf9
Cheolmin Joo <cjfals4539@gmail.com>
2026-02-20 15:33:52 +0900

    LootTagWidget Initialized

---
+++ Source/Team3_Project/Private/UI/Item/LootTagWidget.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Item/LootTagWidget.h"
+#include "Components/TextBlock.h"
+#include "Core/ItemDataSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+
+void ULootTagWidget::InitLootTag(FName InItemID, int32 InQuantity)
+{
+	UGameInstance* GameInstance = UGameplayStatics::GetGameInstance(GetWorld());
+	if (!GameInstance)
+	{
+		return;
+	}
+	UItemDataSubsystem* ItemDataSubsystem = GameInstance->GetSubsystem<UItemDataSubsystem>();
+	if (!ItemDataSubsystem)
+	{
+		return;
+	}
+	FItemData ItemData = ItemDataSubsystem->GetItemDataByID(InItemID);
+
+	if (Txt_ItemName)
+	{
+		Txt_ItemName->SetText(ItemData.DisplayName);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("InitLootTag: Txt_ItemName widget is null"));
+	}
+
+	if (Txt_Quantity)
+	{
+		if (InQuantity > 1)
+		{
+			Txt_Quantity->SetText(FText::Format(NSLOCTEXT("LootTag", "Qty", "x{0}"), InQuantity));
+			Txt_Quantity->SetVisibility(ESlateVisibility::Visible);
+		}
+		else
+		{
+			Txt_Quantity->SetVisibility(ESlateVisibility::Hidden);
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("InitLootTag: Txt_Quantity widget is null"));
+	}
+}
---
+++ Source/Team3_Project/Public/UI/Item/LootTagWidget.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LootTagWidget.generated.h"
+
+class UTextBlock;
+
+UCLASS()
+class TEAM3_PROJECT_API ULootTagWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "LootTag")
+	void InitLootTag(FName InItemID, int32 InQuantity);
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* Txt_ItemName;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* Txt_Quantity;
+	
+};
