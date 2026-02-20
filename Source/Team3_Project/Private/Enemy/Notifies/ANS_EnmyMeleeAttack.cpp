1d3f16450fec0c172a69075f03f32f7269f80465
정찬호_JCH <elvpfhhrm@gmail.com>
2026-02-20 21:19:07 +0900

    Add Enemy Notifies(Source)

---
+++ Source/Team3_Project/Private/Enemy/Notifies/ANS_EnmyMeleeAttack.cpp
@@ -0,0 +1,16 @@
+﻿#include "Enemy/Notifies/ANS_EnmyMeleeAttack.h"
+#include "Enemy/EnemyCharacter.h"
+
+void UANS_EnmyMeleeAttack::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+    Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+}
+void UANS_EnmyMeleeAttack::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+    Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+
+}
+void UANS_EnmyMeleeAttack::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+    Super::NotifyEnd(MeshComp, Animation, EventReference);
+}
---
+++ Source/Team3_Project/Private/Enemy/Notifies/AN_EndAttack.cpp
@@ -0,0 +1,13 @@
+﻿#include "Enemy/Notifies/AN_EndAttack.h"
+#include "Enemy/EnemyCharacter.h"
+
+void UAN_EndAttack::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+    AActor* OwnerActor = MeshComp->GetOwner();
+    if (!OwnerActor) return;
+
+    AEnemyCharacter* EnemyCharacter = Cast<AEnemyCharacter>(OwnerActor);
+    if (!EnemyCharacter) return;
+
+    EnemyCharacter->OnFinishAttack();
+}
